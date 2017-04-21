26c7c3d54326eb80b54be38f0fdd0c989d7b0046
anselmos <anselmos@users.noreply.github.com>
2017-04-21 13:44:05 +0200

    initial add of menu

--- app/src/main/java/com/github/anselmos/popularmovies/MainActivity.java
+++ app/src/main/java/com/github/anselmos/popularmovies/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -54,5 +55,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(detailsIntent);
             }
         });
+    }
+    
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
+        return true;
     }
 }
