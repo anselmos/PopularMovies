5d518766807db78b51e27257e0f3fbf55acadb92
anselmos <anselmos@users.noreply.github.com>
2017-04-21 23:17:46 +0200

    Adds failing initial test for espresso usage.

---
+++ app/src/androidTest/java/com/github/anselmos/popularmovies/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.github.anselmos.popularmovies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.runner.AndroidJUnit4;
+import static org.junit.Assert.*;
+
+/**
+ * Created by anselmos on 21.04.17.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Test
+    public void something(){
+        assertTrue(false);
+    }
+}
