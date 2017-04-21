fffc343faa270525140aa3beb540c8e32bde5924
anselmos <anselmos@users.noreply.github.com>
2017-04-21 12:54:15 +0200

    Adds methods for better usage of Url builder for image Posters.

--- app/src/main/java/com/github/anselmos/popularmovies/utils/PosterUrlBuilder.java
+++ app/src/main/java/com/github/anselmos/popularmovies/utils/PosterUrlBuilder.java
@@ -5,5 +5,24 @@ package com.github.anselmos.popularmovies.utils;
  */
 public class PosterUrlBuilder {
     public static String API = "https://image.tmdb.org/t/p/";
+    public enum ImageSize{
+        SMALL(154), MEDIUM(342), LARGE(780);
     
+        private int value;
+        private ImageSize(int value){
+            this.value = value;
+        }
+    }
+    
+    public String build(final String imageSize, final String imagePath){
+        return API + "w"+ imageSize + imagePath;
+    }
+    
+    public String getSmallImage(final String imagePath){
+        return this.build(String.valueOf(ImageSize.SMALL.value), imagePath);
+    }
+    
+    public String getMediumImage(final String imagePath){
+        return this.build(String.valueOf(ImageSize.MEDIUM.value), imagePath);
+    }
 }
