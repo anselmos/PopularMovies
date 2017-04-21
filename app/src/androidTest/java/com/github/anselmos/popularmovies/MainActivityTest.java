73077ef30a9c8e9eaa437c4be16ce4c946d16b38
anselmos <anselmos@users.noreply.github.com>
2017-04-21 23:37:46 +0200

    Adds mainActivityLayoutVisible test that passes.

--- app/src/androidTest/java/com/github/anselmos/popularmovies/MainActivityTest.java
+++ app/src/androidTest/java/com/github/anselmos/popularmovies/MainActivityTest.java
@@ -3,16 +3,27 @@ package com.github.anselmos.popularmovies;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import static org.junit.Assert.*;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by anselmos on 21.04.17.
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    
+    ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    
     @Test
-    public void something(){
-        assertTrue(false);
+    public void mainAcitivityLayoutVisible() {
+        activityTestRule.launchActivity(new Intent());
+        onView(withId(R.id.poster_grid)).perform(click()).check(matches(isDisplayed()));
     }
 }
