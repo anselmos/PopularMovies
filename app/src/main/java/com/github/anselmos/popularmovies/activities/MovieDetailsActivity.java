0d6331f2acf1f76df5941bd07831e7bf9091ed96
anselmos <anselmos@users.noreply.github.com>
2017-04-22 16:13:27 +0200

    Adds butter knife for view-binding.

--- app/src/main/java/com/github/anselmos/popularmovies/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/github/anselmos/popularmovies/activities/MovieDetailsActivity.java
@@ -9,33 +9,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by anselmos on 08.04.17.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MovieDetailsActivity extends AppCompatActivity {
+    
+    @BindView(R.id.original_title)
+    TextView original_title;
+    
+    @BindView(R.id.plot_synopsis_overview)
+    TextView plot_synopsis_overview;
+    
+    @BindView(R.id.image_thumbnail)
+    ImageView imageView;
+    
+    @BindView(R.id.vote_average)
+    TextView vote_average;
+    
+    @BindView(R.id.release_date)
+    TextView release_date;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details_activity);
+        ButterKnife.bind(this);
         Bundle extras = getIntent().getExtras();
         this.updateMovieDetails(extras);
     }
     
     public void updateMovieDetails(Bundle extras) {
-        TextView original_title = (TextView) this.findViewById(R.id.original_title);
-        original_title.setText(extras.getString("original_title"));
-        System.out.println(extras.getString("release_date"));
+        this.original_title.setText(extras.getString("original_title"));
+        this.plot_synopsis_overview.setText(extras.getString("plot_synopsis_overview"));
+        this.vote_average.setText(extras.getString("vote_average"));
+        this.release_date.setText(extras.getString("release_date"));
         
-        TextView plot_synopsis_overview = (TextView) this.findViewById(R.id.plot_synopsis_overview);
-        plot_synopsis_overview.setText(extras.getString("plot_synopsis_overview"));
-        
-        ImageView imageView = (ImageView) this.findViewById(R.id.image_thumbnail);
         ApiAccess.insertImageInView(this.getApplicationContext(), imageView, extras.getString("image_thumbnail"), ImageSize.MEDIUM);
-        TextView vote_average = (TextView) this.findViewById(R.id.vote_average);
-        vote_average.setText(extras.getString("vote_average"));
-        
-        TextView release_date = (TextView) this.findViewById(R.id.release_date);
-        release_date.setText(extras.getString("release_date"));
     }
 }
